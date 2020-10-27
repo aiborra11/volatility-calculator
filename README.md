f20b775f71e06a1e6d6950f60dbc90c3f515068e
aiborra11 <iborra.11@gmail.com>
2020-10-27 22:14:14 +0100

    to push

---
+++ README.md
@@ -0,0 +1,53 @@
+
+# Volatility Index Calculator
+
+_The scope of this project is to calculate and obtain the historic volatility for any currency, commodity, security, etc._
+
+Risk management is one of the key points at every trading strategy and volatility plays a key role in here. 
+We can use it to expect specific fluctuations in the returns of the underlying asset that might happen on the determined timeframe. 
+
+
+The way I calculate it is throughout logarithmic percentage change of the currency spot price at every minute during X periods of time. 
+
+![volatility_formula](https://github.com/aiborra11/volatility-calculator/output/images/volatility_formula.png)
+
+It makes a lot of sense to me using the natural logarithm to decrease the growth as time goes by. Volatility can't (or shouldn't) grow at a high rate during long periods of time.
+
+
+## Script Execution
+You will need to input a dataframe containing the closing 1 min spot prices  of the asset you are analysing.
+**Make sure you locate the correct csv file into your data folder and name it as "1min_XXXXX".**
+
+Then, execute the script and you will generate a new csv file conatining a new column for the volatility index. 
+Also, you can group the volatility in your desired timeframe (Daily, Hourly, 5min, etc.).
+
+It will automatically detect either you already have volatility and then, avoid calculating it from scratch (time consuming),
+either you are already up to date. 
+
+Finally, you will be able to analyse the volatility and use it for SL/TP calculations, backtest and realise if you are cutting wins/loses, etc.
+
+Here is a sample of the volatility XBT experienced the previous month at the moment of publishing this repo (27/10/2020):
+
+![volatility_index](https://github.com/aiborra11/volatility-calculator/output/images/volatility_index.png)
+
+In this case I calculated the Historic Volatility using a daily basis, but feel free to modify the window param to calculate 
+at a weekly, Daily, Monthly, etc., level. 
+        
+        window:
+            a) Daiy = 1440 (minutes in a day)
+            b) Weekly = 10080 (minutes in a week)
+            ...
+
+### Bonus
+_In case you need the raw data to implement this calculations, I developed a crypto-scraper in this other repository that will
+help you to create and populate your database with any available cryptocurrency in Bitmex. You will also find some other features 
+I considered interesting to develop a better analysis_
+
+https://github.com/aiborra11/crypto-scraper
+
+Soon, I will include the volatility calculator into the crypto-scraper.
+
+
+
+
+
---
+++ volatility.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+import math
+import os
+
+from datetime import datetime
+
+
+
+def volatility(df):
+    """
+    Daily Historical Volatility Index calculated using the logarithmic percentage change taken from spot price
+    of the currency at every minute and converting it to the desired interval (modify the window param in case you
+    want to use a different interval.).
+
+        window:
+            a) Daiy = 1440 (minutes in a day)
+            b) Weekly = 10080 (minutes in a week)
+            ...
+
+        Formula:
+            Stdev(Ln(P1/P0), Ln(P2/P1), ..., Ln(P1440/P1439)) * Sqrt(1440)
+
+    Arguments:
+    ----------
+        df {[DataFrame]} -- dataframe containing the historic 1min closing price.
+
+    Returns:
+    --------
+        {[float]}
+            Volatility percentage of the currency at the closing price calculated after the window historic prices.
+    """
+
+    window = 1440
+
+    prices = pd.DataFrame(df).rename(columns={0: 'Close'})
+    prices['Close'] = prices['Close'].ffill()
+    prices['LagClose'] = prices['Close'].shift(1)
+    volatility = ((math.log(prices['LagClose'] / prices['Close'])).std() * np.sqrt(window))
+    return volatility
+
+
+def time_indexer(df):
+    """
+    Converting timestamp column into datetime format and indexing
+
+    Arguments:
+    ----------
+        df {[DataFrame]} -- dataframe containing the historic data.
+
+    Returns:
+    --------
+        {[DataFrame]}
+            Timestamp indexed as a datetime format.
+    """
+    df['Timestamp'] = df['Timestamp'].map(lambda t: datetime.strptime(str(t), '%Y-%m-%d %H:%M:%S'))
+    return df.set_index('Timestamp')
+
+
+def get_volatility(data, frequency, window=1440):
+    """
+    To avoid recalculating the volatility fot the whole dataframe, we detect the last calculated index position and
+    apply the volatility function for this missing period. In case any is detected we will calculate the volatility for
+    the whole dataframe.
+
+
+    Arguments:
+    ----------
+        df {[DataFrame]} -- dataframe containing the historic 1min closing price.
+        frequency {[str]} -- Timeframe we'd like to receive the data
+        window {[int]} -- Over which period do we want to calculate the volatility
+                        a) Daiy = 1440 (minutes in a day)
+                        b) Weekly = 10080 (minutes in a week)
+                        ...
+    Returns:
+    --------
+        {[DataFrame]}[0]
+            Dataframe containing all the data + the volatility at 1 min intervals.
+            csv file containing all the data + the volatility at 1 min intervals.
+
+        {[DataFrame]}[1]
+            Dataframe containing the volatility grouped at a desired interval
+    """
+
+    # Checking if you have the required file (1min closing price data) to execute this script.
+    valid_files = [file for file in os.listdir("./data.nosync") if file.endswith(".csv")
+                                                                        and file.split('_')[0] == '1min']
+    if valid_files:
+        print('Perfect! You have a file containing 1min closing price data.')
+        print('Now, checking if the file has previous volatility...')
+
+        existing_volatility = [x for x in data.columns if x == 'Volatility']
+
+        # Continue from previous volatility calculation
+        if existing_volatility:
+            print('There is volatility. Checking if an update is needed...')
+            # Finding missing values for volatility so we know the row we should use as a starting point
+            data_nan = data[window:]
+            try:
+                index = data_nan[data_nan['Volatility'].isnull()].index.tolist()[0]
+                print('First NaN value in volatility column is at position: ', index)
+                print('Updating your volatility column...')
+
+                # Creating dataframe for the missing volatility
+                df = pd.DataFrame(data['Timestamp'][(index - window):])
+
+                # Executing volatility function
+                vol = (data['Close'][(index - window):]).rolling(window, min_periods=window).apply(volatility)
+                df = pd.concat([df.reset_index(drop=True), vol.reset_index(drop=True)], axis=1) \
+                                                                                .rename(columns={'Close': 'Volatility'})
+
+                # Merging with original dataframe and calculating volatility column
+                data_vol = data.merge(df, right_on='Timestamp', left_on='Timestamp', how='left')
+                data_vol['Volatility'] = data_vol['Volatility_x'].fillna(0) + data_vol['Volatility_y'].fillna(0)
+
+                # Indexing Timestamp to use the grouper function and obtain data at a desired timeframe
+                data_vol = time_indexer(data_vol)
+
+                data_vol.to_csv(f'./output/1min_general.csv')
+
+                print('Check your output folder!')
+                return data_vol, data_vol['Volatility'].groupby(pd.Grouper(freq=frequency)).mean()
+
+            except:
+                print('Seems you are up to date! We cannot find a valid missing value for volatility.')
+                data = time_indexer(data)
+                return data, data['Volatility'].groupby(pd.Grouper(freq=frequency)).mean()
+
+
+        # Calculate volatility from scratch
+        else:
+            print('Calculating volatility from scratch!')
+
+            #Executing volatility function
+            data['Volatility'] = data['Close'].rolling(window, min_periods=window).apply(volatility)
+
+            #Indexing Timestamp to receive data at a desired timeframe
+            data = time_indexer(data)
+
+            data.to_csv(f'./output/1min_general.csv')
+
+            return data['Volatility'].groupby(pd.Grouper(freq=frequency)).mean()
+
+    else:
+        print('You need to input 1 minute closing price data to run this algorithm')
